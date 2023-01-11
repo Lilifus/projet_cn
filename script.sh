d20c60a6b5e2189ccadb31f16a567401cf72bc30
Lilifus <zakaria.ejjed@gmail.com>
2023-01-11 13:50:20 +0100

    UPDATE-taskset on core 1 instead of core 7
    Taskset in script has been changed to core 1 (because the laptop only
    have 2 cores and can't handle taskset -c 7) .

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@ max=17
 rm -Rf DGBTRF.dat DGBTRI.dat DGBSV.dat
 for ((i = 4, j = 1 ; j<=$max ; i*=2, j++)); do
     echo "$j / $max , arg = $i"
-    output=$( make -B tpPoisson1D_direct; taskset -c 7 ./bin/tpPoisson1D_direct $i )
+    output=$( make -B tpPoisson1D_direct; taskset -c 1 ./bin/tpPoisson1D_direct $i )
     echo $i $(echo "$output" | grep -i dgbtrf) >> DGBTRF.dat
     echo $i $(echo "$output" | grep -i tridiag) >> DGBTRI.dat
     echo $i $(echo "$output" | grep -i dgbsv) >> DGBSV.dat
